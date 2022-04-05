a0319adc7de48a7efa39a2d18e4e839c92358748
Kevin Faaborg <kevin@ara.one>
2022-04-05 13:57:57 -0600

    minimal includes

--- src/App.js
+++ src/App.js
@@ -13,10 +13,32 @@ function App() {
 	return (
 		<div className="App">
 			<p>Hello, radical-mint!</p>
-			<p>Date 2022apr5, Version 10</p>
+			<p>Date 2022apr5, Version 11</p>
 			<Wallet />
 		</div>
 	);
 }
 
 export default App;
+
+
+/*
+moved here from package.json
+
+    "@alch/alchemy-web3": "^1.3.0",
+
+    "dotenv": "^16.0.0",
+
+    "assert": "^2.0.0",
+    "crypto": "^1.0.1",
+    "http": "0.0.1-security",
+    "https": "^1.0.0",
+    "stream": "0.0.2",
+    "util": "^0.12.4",
+
+
+
+
+
+
+*/
