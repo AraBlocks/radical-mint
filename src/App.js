c209f79a2ff98d9f8b2fa48ebab49be028f25bd5
Kevin Faaborg <kevin@ara.one>
2022-04-05 16:05:38 -0600

    older package versions

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Wallet from './components/wallet';
-
-
-
-//uncomment this line to see all the errors!
-//const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-
-
-
-function App() {
-	return (
-		<div className="App">
-			<p>Hello, radical-mint!</p>
-			<p>Date 2022apr5, Version 12</p>
-			<Wallet />
-		</div>
-	);
-}
-
-export default App;
-
-
 /*
-moved here from package.json
 
     "@alch/alchemy-web3": "^1.3.0",
+    "@testing-library/jest-dom": "^5.16.2",
+    "@testing-library/react": "^12.1.4",
+    "@testing-library/user-event": "^13.5.0",
+    "react": "^17.0.2",
+    "react-device-detect": "^2.1.2",
+    "react-dom": "^17.0.2",
+    "react-scripts": "5.0.0",
+    "web-vitals": "^2.1.4"
 
     "dotenv": "^16.0.0",
 
@@ -42,3 +28,22 @@ moved here from package.json
 
 
 */
+
+
+//uncomment this line to see all the errors!
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+
+
+
+function App() {
+	return (
+		<div className="App">
+			<p>Hello, radical-mint!</p>
+			<p>Date 2022apr5, Version 16</p>
+			<Wallet />
+		</div>
+	);
+}
+
+export default App;
+
