e3f6024d90f905669241bc0cddbc9f355be01d44
Kevin Faaborg <kevin@ara.one>
2022-04-08 13:33:12 -0600

    alchemy key in environment variable

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ import Wallet from './components/wallet';
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 
 
+//troublesome area below, trying to get it out of comments
+/*
+const alchemyKey = "https://eth-mainnet.alchemyapi.io/v2/" + "GetFromEnvironmentVariables";//TODO
+const web3 = createAlchemyWeb3(alchemyKey);
+
+let contractABI = require('./contract-abi.json');
+// const contractAddress = '0xd80B07293C85C91A221B0538369994C70fb5cdBe';//rinkeby
+const contractAddress = '0x88954a16b93f296d3d993793143e2dcbc32222b2';//mainnet
+
+export const CollectionContract = new web3.eth.Contract(contractABI, contractAddress);
+CollectionContract.handleRevert = true;
+*/
+
+
 
 function App() {
 	return (
 		<div className="App">
 			<p>Hello, radical-mint!</p>
-			<p>Date 2022apr8, Version 19</p>
+			<p>Date 2022apr8, Version 20</p>
 			<Wallet />
 		</div>
 	);
--- src/util/interact.js
+++ src/util/interact.js
@@ -26,7 +26,8 @@ export function myFunFunction() {
 	console.log(process.env.REACT_APP_KEY);
 	console.log(process.env.REACT_APP_SECOND_KEY);
 	console.log(process.env.REACT_APP_KEY3);
-	console.log("^ hopefully you got three environment variables");
+	console.log(process.env.REACT_APP_ALCHEMY.length);
+	console.log("^ hopefully you got three environment variables, and a length");
 
 	return "return value";
 }
@@ -69,19 +70,6 @@ export const getCurrentWalletConnected = async () => {
 };
 
 
-//troublesome area below, trying to get it out of comments
-/*
-const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-const alchemyKey = "https://eth-mainnet.alchemyapi.io/v2/" + "GetFromEnvironmentVariables";//TODO
-const web3 = createAlchemyWeb3(alchemyKey);
-
-let contractABI = require('./contract-abi.json');
-// const contractAddress = '0xd80B07293C85C91A221B0538369994C70fb5cdBe';//rinkeby
-const contractAddress = '0x88954a16b93f296d3d993793143e2dcbc32222b2';//mainnet
-
-export const CollectionContract = new web3.eth.Contract(contractABI, contractAddress);
-CollectionContract.handleRevert = true;
-*/
 
 
 export const connectWallet = async () => {
