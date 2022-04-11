f40fd8df070a2642670e99b8a3e006f0e3d9a111
Kevin Faaborg <kevin@ara.one>
2022-04-11 12:39:34 -0600

    before single filing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+
 import logo from './logo.svg';
 import './App.css';
 import Wallet from './components/wallet';
 
+const onMainnet = true;//change to true to deploy
+const onRinkeby = !onMainnet;
+
+const contractAddressOnRinkeby = "0xd80B07293C85C91A221B0538369994C70fb5cdBe";
+const contractAddressOnMainnet = "0x88954a16b93f296d3d993793143e2dcbc32222b2";
+const contractAddress = onMainnet ? contractAddressOnMainnet : contractAddressOnRinkeby;
+
+const log = console.log;
 
 
-//this works now
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-
-
-//troublesome area below, trying to get it out of comments
-/*
-const alchemyKey = "https://eth-mainnet.alchemyapi.io/v2/" + "GetFromEnvironmentVariables";//TODO
+const alchemyKey = "https://eth-mainnet.alchemyapi.io/v2/" + process.env.REACT_APP_ALCHEMY;
 const web3 = createAlchemyWeb3(alchemyKey);
-
-let contractABI = require('./contract-abi.json');
-// const contractAddress = '0xd80B07293C85C91A221B0538369994C70fb5cdBe';//rinkeby
-const contractAddress = '0x88954a16b93f296d3d993793143e2dcbc32222b2';//mainnet
-
+let contractABI = require('./util/contract-abi.json');
 export const CollectionContract = new web3.eth.Contract(contractABI, contractAddress);
 CollectionContract.handleRevert = true;
-*/
+
+
+
+
 
 
 
@@ -27,7 +30,7 @@ function App() {
 	return (
 		<div className="App">
 			<p>Hello, radical-mint!</p>
-			<p>Date 2022apr10, Version 21</p>
+			<p>Date 2022apr11, Version 24</p>
 			<Wallet />
 		</div>
 	);
